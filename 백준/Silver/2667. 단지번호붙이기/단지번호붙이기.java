86bc192f824f54086a5c6daf7eea4a14288aebe6
Ryujy <63836145+Ryujy@users.noreply.github.com>
2023-10-25 20:51:18 +0900

    [Silver I] Title: 단지번호붙이기, Time: 80 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,75 +1,75 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-	static int arr[][];
-	static boolean visit[][];
-	static int dirX[] = {0, 0, -1, 1};
-	static int dirY[] = {-1, 1, 0, 0};
+    
+    static int N;
+    static char[][] map;
+    static boolean[][] v;
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        v = new boolean[N][N];
+        
+        for (int i=0; i<N; i++) {
+        	String str = br.readLine();
+            for (int j=0; j<N; j++) {
+                map[i][j] = str.charAt(j);
+            }
+        }
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                if(map[i][j] == '1' && !v[i][j]) {
+                    int res = bfs(i,j);
+                    list.add(res);
+                }
+            }
+        }
+        Collections.sort(list);
+        sb.append(list.size()).append('\n');
+        for (int l : list) {
+        	sb.append(l).append('\n');
+        }
+        System.out.println(sb);
+    }
 
-	static int count = 0, number = 0;
-	static int nowX, nowY, N;
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		List<Integer> list = new ArrayList<>();
-
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N][N];
-		visit = new boolean[N][N];
-
-		for(int i=0; i<N; i++) {
-			String str = br.readLine();
-
-			for(int j=0; j<N; j++) {				
-				arr[i][j] = Character.getNumericValue(str.charAt(j));
-			}
-		}
-
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-
-				if(visit[i][j] == false && arr[i][j] == 1) {
-					count = 0;
-					number++;
-					DFS(i, j);	
-					list.add(count);
-				}
-
-			}
-		}
-
-		Collections.sort(list);
-		bw.append(number + "\n");
-		for(int num : list) {
-			bw.append(num + "\n");
-		}
-
-		bw.flush();
-		bw.close();		
-	} // End of main
-
-	static void DFS(int x, int y) {
-		visit[x][y] = true;
-		arr[x][y] = number;
-		count ++;
-
-		for(int i=0; i<4; i++) {
-			nowX = dirX[i] + x;
-			nowY = dirY[i] + y;
-
-			if(Range_check() && visit[nowX][nowY] == false && arr[nowX][nowY] == 1) {
-				visit[nowX][nowY] = true;
-				arr[nowX][nowY] = number;
-
-				DFS(nowX, nowY);
-			}
-		}		
-
-	} // End of DFS
-
-	static boolean Range_check() {
-		return (nowX >= 0 && nowX < N && nowY >= 0 && nowY < N);
-	} // End of Range_check
-} // End of class
+    private static int bfs(int i, int j) {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {i, j});
+        v[i][j] = true;
+        int cnt = 0;
+        
+        while(!q.isEmpty()) {
+            int[] temp = q.poll();
+            cnt += 1;
+            
+            for (int d=0; d<4; d++) {
+            	int nr = temp[0] + dr[d];
+                int nc = temp[1] + dc[d];
+                
+                if(nr > -1 && nr < N && nc > -1 && nc < N && !v[nr][nc]) {
+                	if (map[nr][nc] == '1') {
+                		q.offer(new int[] {nr, nc});
+                		v[nr][nc] = true;
+                	} else {
+                		continue;
+                	}
+                }
+            }
+        }
+        return cnt;
+    }
+}
