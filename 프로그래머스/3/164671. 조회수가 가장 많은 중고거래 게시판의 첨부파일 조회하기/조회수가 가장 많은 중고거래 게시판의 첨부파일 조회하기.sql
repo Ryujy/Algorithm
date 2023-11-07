f786edf2bde82cc000c969c5f20925cb5e328349
Ryujy <63836145+Ryujy@users.noreply.github.com>
2023-11-07 17:58:39 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,9 +1,2 @@
 -- 코드를 입력하세요
-SELECT CONCAT('/home/grep/src/',board_id,'/',file_id,file_name,file_ext) AS FLIE_PATH
-FROM USED_GOODS_FILE
-WHERE BOARD_ID = (SELECT BOARD_ID
-    FROM USED_GOODS_BOARD B
-    ORDER BY VIEWS DESC
-    LIMIT 1)
-order by file_id desc
-
+SELECT
