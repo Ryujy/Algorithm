7d48c636bfcc7d275ab6537408a4b6a26c711ebb
Ryujy <63836145+Ryujy@users.noreply.github.com>
2023-12-09 00:17:34 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 596 ms, Memory: 56752 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,44 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
+// 백준 - 계단오르기
 public class Main {
-	static int N; // 수의 개수
-	static int T; // tc의 수
-	static int[] nums; // N개의 수
-	static int[] prefixSum; // 누적합 배열
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		StringBuilder sb = new StringBuilder();
-
-		N = Integer.parseInt(st.nextToken());
-		T = Integer.parseInt(st.nextToken());
-
-		nums = new int[N + 1];
-		prefixSum = new int[N + 1];
-
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); //수의 개수
+		int M = Integer.parseInt(st.nextToken()); //tc 횟수
+		
+		int[] sum = new int[N+1]; // 누적합
+		
 		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i < N + 1; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-			prefixSum[i] = prefixSum[i - 1] + nums[i];
+		for (int i=1; i<N+1; i++) {
+			sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
 		}
 		
-		for (int tc = 0; tc < T; tc++) {
+		for (int tc=0; tc<M; tc++) {
 			st = new StringTokenizer(br.readLine());
-			int i = Integer.parseInt(st.nextToken());
-			int j = Integer.parseInt(st.nextToken());
-
-			int sum = prefixSum[j] - prefixSum[i - 1];
-
-			sb.append(sum).append("\n");
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			sb.append(sum[end] - sum[start-1]).append("\n");
 		}
-		System.out.println(sb.toString());
+		System.out.println(sb);
 	}
-
 }
