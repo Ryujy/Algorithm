266cb0784c73c14249e4fc1f6c22e6462ad2af3d
Ryujy <63836145+Ryujy@users.noreply.github.com>
2024-08-25 23:59:55 +0900

    [Silver III] Title: 크리스마스 선물, Time: 444 ms, Memory: 40872 KB -BaekjoonHub

---
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+import java.util.function.Function;
+
+public class Main {
+    private static final int EMPTY = -1;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Function<String,Integer> stoi = Integer::parseInt;
+        int n = stoi.apply(st.nextToken());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine());
+            int cnt = stoi.apply(st.nextToken());
+            if(cnt == 0){
+                if(!pq.isEmpty()){
+                    System.out.println(pq.poll());
+                }else{
+                    System.out.println(EMPTY);
+                }
+                continue;
+            }
+            for(int j = 0 ; j < cnt ; j++){
+                pq.offer(stoi.apply(st.nextToken()));
+            }
+        }
+    }
+}
