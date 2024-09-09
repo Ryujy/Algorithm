cdf8ab36e15d4875d1c9b4174a9dd811ffb40781
Ryujy <63836145+Ryujy@users.noreply.github.com>
2024-09-09 23:40:22 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 27.09 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,29 +1,28 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        
         int didx = n-1; // 배달 idx
         int pidx = n-1; // 픽업 idx
-        long answer = 0; //이동 거리
         
         while(true){
-            if (didx < 0 && pidx < 0){
-                break;
-            }
+            if (didx < 0 && pidx < 0) break;
+            int cur = cap; // 트럭에 싣고 갈 짐
+            int pick  = 0;
             int d = -1;
             int p = -1;
-            int bring = cap; // 상자 개수
-            int pick = 0;
-            while(bring > 0 && didx != -1){
-                if (deliveries[didx] > 0){
-                    d = d > didx ? d : didx;
+            while(cur > 0 && didx != -1){
+                if(deliveries[didx] > 0){
+                    d = d > didx ? d : didx; // 이동 거리 갱신
                     deliveries[didx]--;
-                    bring--;
-                } else {
+                    cur--;
+                } else  {
                     didx--;
                 }
             }
             while(pick < cap && pidx != -1){
                 if (pickups[pidx] > 0){
-                    p = p > pidx ? p : pidx;
+                    p = p > pidx ? p : pidx; // 이동 거리 갱신
                     pickups[pidx]--;
                     pick++;
                 } else {
