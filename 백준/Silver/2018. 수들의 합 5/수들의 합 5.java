7dcc175ff3308d7779623dd2d2342615990bd9d6
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-01-09 16:40:05 +0900

    [Silver V] Title: 수들의 합 5, Time: 144 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// BOJ_2018 수들의 합 5
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int p1 = 1, p2 = 1;
+        int sum = p1, cnt = 1;
+        for (int i=1; i<n; i++){
+            while(p1 < n){
+                if (sum == n){
+                    cnt++;
+                    sum -= p1++;
+                    sum += ++p2;
+                } else if(sum < n) {
+                    sum += ++p2;
+                } else {
+                    sum -= p1++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+}
