2e229860ad00da7e85f8c2344cf34177e31da494
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-01-18 03:41:55 +0900

    [Silver I] Title: 회전 초밥, Time: 124 ms, Memory: 16468 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,50 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.StringTokenizer;
 
-// 백준 - 회전 초밥
+// BOJ_2531 회전 초밥
 public class Main {
-	
-	static int N, d, k, c;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken()); // 접시의 수
-		d = Integer.parseInt(st.nextToken()); // 초밥의 가짓수
-		k = Integer.parseInt(st.nextToken()); // 연속해서 먹는 접시의 수
-		c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
-		
-		int[] chobap = new int[N];
-		
-		for (int i=0; i<N; i++) {
-			chobap[i] = Integer.parseInt(br.readLine());
-		}
-		
-		int max = Integer.MIN_VALUE;
-		// 0번 그릇 부터 N-1번 그릇까지 반복
-		for (int i=0; i<N; i++) {
-			int[] eat = new int[d+1]; // 먹은 초밥의 종류 기록
-			int cnt = 1;
-			eat[c]++; // 쿠폰 기록
-			for (int j=i; j<i+k; j++) { // k번 연속 먹는다.
-				if (eat[chobap[j%N]] == 0) {// 해당 종류를 먹은 적이 없다.
-					cnt++;
-					eat[chobap[j%N]]++;
-				}
-			}
-			
-			max = max<cnt?cnt:max; //최댓값 갱신
-		}
-		
-		System.out.println(max);
-	}
-		
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 접시의 수
+        int d = Integer.parseInt(st.nextToken()); // 초밥의 가짓수
+        int k = Integer.parseInt(st.nextToken()); // 연속 접시 수
+        int c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
+
+        int[] kind = new int[d+1];
+        kind[c] = 1;
+        int[] chobob = new int[n+k];
+        for (int i = 0; i<n; i++){
+            chobob[i] = Integer.parseInt(br.readLine());
+        }
+        for (int i = 0; i<k; i++){
+            chobob[n+i] = chobob[i];
+        }
+
+        int cnt = 1, max = 0;
+        int lt = 0, rt = 0;
+        while(rt<n+k){
+            if(kind[chobob[rt]] == 0) {
+                cnt++;
+            }
+            kind[chobob[rt]]++;
+            if(rt-lt+1 == k){
+                max = Math.max(cnt, max);
+
+                kind[chobob[lt]]--;
+                if (kind[chobob[lt]] == 0) cnt--;
+                lt++;
+            }
+            rt++;
+        }
+        System.out.println(max);
+
+    }
 }
