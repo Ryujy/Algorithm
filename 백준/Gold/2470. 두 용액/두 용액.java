6f6b758a00237aab2eb7b9c76204535e1d871399
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-01-19 16:57:04 +0900

    [Gold V] Title: 두 용액, Time: 252 ms, Memory: 32164 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,45 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// BOJ_2470 두 용액
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		
-		int i = 0;
-		int j = arr.length - 1;
-		
-		int gap = Integer.MAX_VALUE;
-		int ans1 = 0;
-		int ans2 = 0;
-		
-		int temp;
-		int sum;
-		while (i < j) {
-			sum = arr[i] + arr[j];
-			temp = Math.abs(sum);
-			if (temp < gap) {
-				gap = temp;
-				ans1 = arr[i];
-				ans2 = arr[j];
-			}
-			if (sum > 0)
-				j--;
-			else
-				i++;
-		}
-		
-		System.out.println(ans1 + " " + ans2);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        // 특성값이 0에 가장 가까운 용액을 만들어내는 두 용액의 특성값을 출력한다. (특성값의 오름차순)
+        Arrays.sort(arr);
+
+        int lt = 0, rt = n-1;
+        int sum;
+        int temp = Integer.MAX_VALUE;
+        int ans1 = 0, ans2 = 0;
+        while(lt < rt){
+            sum = arr[lt] + arr[rt];
+            if(Math.abs(sum) < temp){
+                temp = Math.abs(arr[lt]+arr[rt]);
+                ans1 = arr[lt];
+                ans2 = arr[rt];
+            }
+            // 어떤 경우에 lt와 rt를 각각 움직여야하는지 감을 잡기 어려웠다.
+            if(sum > 0) rt--;
+            else lt++;
+        }
+        System.out.println(ans1+" "+ans2);
+    }
 }
