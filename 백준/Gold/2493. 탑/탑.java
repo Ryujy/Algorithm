f3d4e80c1eabba54b8239beeda30172c22fcdeeb
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-01-26 18:37:49 +0900

    [Gold V] Title: 탑, Time: 720 ms, Memory: 118520 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,28 +1,43 @@
-import java.io.*;
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+// BOJ_2493 탑
 public class Main {
-    
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        Stack<int[]> stack = new Stack<>();
-        for(int i = 1; i <= n; i++) {
-            int top = Integer.parseInt(st.nextToken());
-            while(!stack.isEmpty()) {
-                if(stack.peek()[1] >= top) {
-                    System.out.print(stack.peek()[0] + " ");
-                    break;
-                }
-                stack.pop();
-            }
-            if(stack.isEmpty()) {
-                System.out.print("0 ");
-            }
-            stack.push(new int[] {i, top}); 
+    static class top{
+        int height;
+        int idx;
+
+        top (int height, int idx){
+            this.height = height;
+            this.idx =idx;
         }
     }
-}    
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        Stack<top> stack = new Stack<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i=0; i<n; i++){
+            int value = Integer.parseInt(st.nextToken());
+            while(!stack.isEmpty() && stack.peek().height < value){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                sb.append(0).append(' ');
+            } else {
+                sb.append(stack.peek().idx).append(" ");
+            }
+            stack.push(new top(value, i+1));
+        }
+        System.out.println(sb);
+    }
+}
