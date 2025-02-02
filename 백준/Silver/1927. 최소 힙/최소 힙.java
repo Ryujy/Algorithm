ff557e4ef7712c8b0df11dad26f22b4384b3a402
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-02-02 23:01:19 +0900

    [Silver II] Title: 최소 힙, Time: 1088 ms, Memory: 29652 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,43 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-// 백준 - 최소 힙
 public class Main {
-
-	static int N, M;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		StringBuilder sb = new StringBuilder();
-		
+		PriorityQueue<Integer> queue = new PriorityQueue<>();
 		int N = Integer.parseInt(br.readLine());
-		for (int i=0; i<N; i++) {
-			int x = Integer.parseInt(br.readLine());
-			if (x == 0) {
-				if (pq.isEmpty()) {
-					sb.append(0);
-				}else {
-					sb.append(pq.poll());
-				}
-				sb.append("\n");
-			} else {
-				pq.offer(x);
+		
+		for(int i=0; i<N; i++) {
+			int val = Integer.parseInt(br.readLine());
+			
+			if(val == 0) {
+				if(queue.isEmpty())
+					System.out.println("0");
+				else
+					System.out.println(queue.poll());
 			}
+			else
+				queue.add(val);
 		}
-		
-		System.out.println(sb);
 	}
-		
 }
