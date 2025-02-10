e2c64b97b30285904056fd5d70e0b2de68b1caa6
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-02-10 16:48:45 +0900

    [Silver I] Title: 그림, Time: 324 ms, Memory: 44532 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,90 +1,70 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	static int n, m;
-	static int maxSize;
-	static int[][] map;
-	static boolean[][] v;
-	static int[] dr = {-1,1,0,0};
-	static int[] dc = {0,0,-1,1};
-	
-	public static class Data{
-		int r;
-		int c;
-		
-		public Data(int r, int c) {
-			super();
-			this.r = r;
-			this.c = c;
-		}
-	}
+    static int n, m;
+    static int[][] map;
+    static boolean[][] visit;
+    static int[] dr = {-1, 1, 0, 0}; // 상하좌우
+    static int[] dc = {0, 0, -1, 1};
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken()); //세로
-		m = Integer.parseInt(st.nextToken()); //가로 
-		
-		map = new int[n][m];
-		v = new boolean[n][m];
-		for (int i=0; i<n; i++) {
-			st= new StringTokenizer(br.readLine());
-			for (int j=0; j<m; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		// 그림의 개수, 넓이
-		maxSize = 0;
-		int cnt = 0;
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<m; j++) {
-				if (map[i][j] == 1 && !v[i][j]) {
-					bfs(i,j);
-					cnt++;
-				}
-			}
-		}
-		System.out.println(cnt);
-		System.out.println(maxSize);
-	}
-	
-	static void bfs(int r, int c) {
-		int size = 0;
-		ArrayDeque<Data> q = new ArrayDeque<>();
-		
-		q.offer(new Data(r, c));
-		v[r][c] = true;
-		
-		while(!q.isEmpty()) {
-			Data cur = q.poll();
-			size++;
-			
-			for (int d=0; d<4; d++) {
-				int nr = cur.r + dr[d];
-				int nc = cur.c + dc[d];
-				
-				if (nr > -1 && nr < n && nc > -1 && nc < m) {
-					if (!v[nr][nc] && map[nr][nc] == 1) {
-						q.offer(new Data(nr, nc));
-						v[nr][nc] = true;
-					}
-				}
-			}
-		}
-		maxSize = (maxSize > size)?maxSize:size;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map  =  new int[n][m];
+        visit  =  new boolean[n][m];
+
+        for (int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<m; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int cnt = 0, max = 0;
+        for (int i=0; i<n; i++){
+            for (int j=0; j<m; j++){
+                if(map[i][j] == 1 && !visit[i][j]){
+                    int res = bfs(i, j);
+                    cnt++;
+                    max = Math.max(max, res);
+                }
+            }
+        }
+        System.out.println(cnt);
+        System.out.println(max);
+    }
+
+    private static int bfs(int r, int c){
+        ArrayDeque<int[]> que = new ArrayDeque<>();
+        que.offer(new int[] {r, c});
+        visit[r][c] = true;
+
+        int cnt = 0;
+        while (!que.isEmpty()){
+            int[] cur = que.pop();
+            int cr = cur[0];
+            int cc = cur[1];
+
+            for (int d=0; d<4; d++){
+                int nr = cr + dr[d];
+                int nc = cc + dc[d];
+
+                if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+                if (map[nr][nc] == 1 & !visit[nr][nc]){
+                    que.offer(new int[] {nr, nc});
+                    visit[nr][nc] = true;
+                }
+            }
+            cnt++;
+        }
+        return cnt;
+    }
 }
