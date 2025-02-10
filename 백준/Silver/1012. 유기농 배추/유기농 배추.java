5ddf4d54a5e124d68dc082f1b1885e5718b37fb5
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-02-10 17:08:23 +0900

    [Silver II] Title: 유기농 배추, Time: 100 ms, Memory: 13680 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,81 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
+public class Main { //BOJ_1012_유기농배추
 
-public class Main {
-	
-	static int M,N,K;
-	static int[][] map;
-	static boolean[][] visit;
-	static int[] dx = {-1, 1, 0, 0}; 
-	static int[] dy = {0, 0, -1, 1};
-	static int cnt;
+    static int n, m;
+    static int[][] map;
+    static boolean[][] visit;
+    static int[] dr = {-1, 1, 0, 0}; // 상하좌우
+    static int[] dc = {0, 0, -1, 1};
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1; tc < T+1; tc++) {
-			st = new StringTokenizer(br.readLine());
-			
-			M = Integer.parseInt(st.nextToken()); //가로 x
-			N = Integer.parseInt(st.nextToken()); //세로 y
-			K = Integer.parseInt(st.nextToken()); // 배추 위치 개수
-			
-			map = new int[N][M];
-			visit = new boolean[N][M];
-			
-			for (int k=0; k<K; k++) { //배추 위치 기록
-				st = new StringTokenizer(br.readLine());
-				int y = Integer.parseInt(st.nextToken()); //M
-				int x = Integer.parseInt(st.nextToken()); //N
-				
-				map[x][y] = 1;
-			}
-			
-			cnt = 0;
-			
-			for (int i=0; i<N; i++) {
-				for (int j=0; j<M; j++) {
-					if (map[i][j] == 1 && !visit[i][j]) {
-						bfs(i,j);
-						cnt++;
-					}
-				}
-			}
-			
-			System.out.println(cnt);
-		}// end tc
-	}
-	
-	static void bfs(int sx, int sy) {
-		Queue<int[]> q = new ArrayDeque<>();
-		
-		q.offer(new int[] {sx, sy});
-		visit[sx][sy] = true;
-		
-		while(!q.isEmpty()) {
-			int[] temp = q.poll();
-			
-			for (int i=0; i<4; i++) {
-				int nx = temp[0]+dx[i];
-				int ny = temp[1]+dy[i];
-				
-				if (nx >= 0 && nx < N && ny >= 0 && ny < M) { //맵 안에서
-					if (map[nx][ny] == 1 && !visit[nx][ny]) { //배추라면
-						q.offer(new int[] {nx, ny});
-						visit[nx][ny] = true;
-					}
-				}
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int T = Integer.parseInt(br.readLine()); //tc
+
+        StringTokenizer st;
+        for (int tc=1; tc<T+1; tc++){
+            st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken()); // 가로 , 열 c
+            n = Integer.parseInt(st.nextToken()); // 세로 ,행 r
+            int k = Integer.parseInt(st.nextToken()); // 배추가 심어진 위치의 개수
+
+            map = new int[n][m];
+            visit = new boolean[n][m];
+
+            for (int i=0; i<k; i++){
+                st = new StringTokenizer(br.readLine());
+                int c = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+
+                map[r][c] = 1;
+            }
+
+            int cnt = 0;
+            for (int i=0;i<n;i++){
+                for (int j=0; j<m; j++){
+                    if(map[i][j] == 1 & !visit[i][j]){
+                        bfs(i, j);
+                        cnt++;
+                    }
+                }
+            }
+            System.out.println(cnt);
+        } // tc end
+    }
+
+    private static void bfs(int r, int c){
+        ArrayDeque<int[]> que = new ArrayDeque<>();
+        que.offer(new int[] {r, c});
+        visit[r][c] = true;
+
+        while (!que.isEmpty()){
+            int[] cur = que.pop();
+            int cr = cur[0];
+            int cc = cur[1];
+
+            for (int d=0; d<4; d++){
+                int nr = cr + dr[d];
+                int nc = cc + dc[d];
+
+                if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+                if (map[nr][nc] == 1 & !visit[nr][nc]){
+                    que.offer(new int[] {nr, nc});
+                    visit[nr][nc] = true;
+                }
+            }
+        }
+    }
 }
