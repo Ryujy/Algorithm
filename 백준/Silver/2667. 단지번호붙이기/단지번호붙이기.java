35af66f7b106cfbfb6fbc6da52dcfca4003eed03
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-02-12 17:49:25 +0900

    [Silver I] Title: 단지번호붙이기, Time: 76 ms, Memory: 11764 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,75 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-public class Main {
-    
-    static int N;
+public class Main { // BOJ_2667_단지번호붙이기
+
     static char[][] map;
-    static boolean[][] v;
+    static boolean[][] visit;
+    static int n;
     static int[] dr = {-1,1,0,0};
     static int[] dc = {0,0,-1,1};
-    static StringBuilder sb = new StringBuilder();
-    
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new char[N][N];
-        v = new boolean[N][N];
-        
-        for (int i=0; i<N; i++) {
-        	String str = br.readLine();
-            for (int j=0; j<N; j++) {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine()); // 지도의 크기
+
+        map = new char[n][n];
+        visit = new boolean[n][n];
+
+        for (int i=0; i<n; i++){
+            String str = br.readLine();
+            for (int j=0; j<n; j++){
                 map[i][j] = str.charAt(j);
             }
-        }
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<N; j++) {
-                if(map[i][j] == '1' && !v[i][j]) {
-                    int res = bfs(i,j);
-                    list.add(res);
+        } // map end
+
+        ArrayList<Integer> areas = new ArrayList<>();
+
+        int cnt = 0;
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                int area = 0;
+                if (map[i][j] == '1' && !visit[i][j]){
+                    area = bfs(i, j);
+                    areas.add(area);
+                    cnt++;
                 }
             }
         }
-        Collections.sort(list);
-        sb.append(list.size()).append('\n');
-        for (int l : list) {
-        	sb.append(l).append('\n');
+        Collections.sort(areas);
+        System.out.println(cnt);
+        for (int a : areas){
+            System.out.println(a);
         }
-        System.out.println(sb);
     }
 
-    private static int bfs(int i, int j) {
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[] {i, j});
-        v[i][j] = true;
-        int cnt = 0;
-        
-        while(!q.isEmpty()) {
-            int[] temp = q.poll();
-            cnt += 1;
-            
-            for (int d=0; d<4; d++) {
-            	int nr = temp[0] + dr[d];
-                int nc = temp[1] + dc[d];
-                
-                if(nr > -1 && nr < N && nc > -1 && nc < N && !v[nr][nc]) {
-                	if (map[nr][nc] == '1') {
-                		q.offer(new int[] {nr, nc});
-                		v[nr][nc] = true;
-                	} else {
-                		continue;
-                	}
+    private static int bfs(int r, int c){
+        ArrayDeque<int[]> que = new ArrayDeque<>();
+        que.offer(new int[] {r, c});
+        visit[r][c] = true;
+
+        int area = 0;
+        while(!que.isEmpty()){
+            int[] tmp = que.poll();
+            area++;
+            for (int d=0; d<4; d++){
+                int nr = tmp[0] + dr[d];
+                int nc = tmp[1] + dc[d];
+
+                if(nr > -1 && nr < n && nc > -1 && nc < n && !visit[nr][nc]) {
+                    if (map[nr][nc] == '0') continue;
+                    que.offer(new int[] {nr, nc});
+                    visit[nr][nc] = true;
                 }
             }
         }
-        return cnt;
+        return area;
     }
 }
