d10b8df26b3447f37c919257fa9302248859e343
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-02-19 10:20:56 +0900

    [Gold V] Title: 상범 빌딩, Time: 112 ms, Memory: 14820 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -1,108 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
+public class Main { // BOJ_6593_상범빌딩
 
-	static int L,R,C;
-	static char[][][] map;
-	static int[] dr = {-1,1,0,0,0,0};
-	static int[] dc = {0,0,-1,1,0,0};
-	static int[] dz = {0,0,0,0,1,-1};
-	static ArrayDeque<Data> q = new ArrayDeque<>();
-	static StringBuilder sb = new StringBuilder();
-	
-	static class Data{
-		int r;
-		int c;
-		int z;
-		int time;
-		
-		public Data(int z, int r, int c, int time) {
-			this.r = r;
-			this.c = c;
-			this.z = z;
-			this.time = time;
-		}
-	}
+    static char[][][] map;
+    static boolean[][][] visited;
+    static ArrayDeque<int[]> que = new ArrayDeque<>();
+    static int[] dr = {1, -1, 0, 0, 0, 0}; //동서남북상하
+    static int[] dc = {0, 0, 1, -1, 0, 0}; //동서남북상하
+    static int[] dl = {0, 0, 0, 0, 1, -1}; //동서남북상하
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		while(true) {
-			st = new StringTokenizer(br.readLine());
-			L = Integer.parseInt(st.nextToken());
-			R = Integer.parseInt(st.nextToken());
-			C = Integer.parseInt(st.nextToken());
-			
-			map = new char[L][R][C];
-			
-			if (L == 0 && R == 0 && C == 0) {
-				break;
-			}
-			
-			q.clear();
-			for (int i = 0; i<L; i++) {
-				for (int j = 0; j<R+1; j++) {
-					String str = br.readLine();
-					if (str.equals("")) continue;
-					for (int k = 0; k<C; k++) {
-						map[i][j][k] = str.charAt(k);
-						if (map[i][j][k] == 'S') {
-							q.offer(new Data(i,j,k,0));
-							map[i][j][k] = '#';
-						}
-					}
-				}
-			}
-			
-			bfs();
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	private static void bfs() {
-		
-		while(!q.isEmpty()) {
-			Data cur = q.poll();
-			int cz = cur.z;
-			int cr = cur.r;
-			int cc = cur.c;
-			
-			for (int d=0; d<6; d++) {
-				int nz = cz + dz[d];
-				int nr = cr + dr[d];
-				int nc = cc + dc[d];
-				
-				if (isValid(nz, nr, nc)) {
-					if (map[nz][nr][nc] == 'E') {
-						sb.append("Escaped in ").append(cur.time+1)
-						.append(" minute(s).").append('\n');
-						return;
-					}
-					q.offer(new Data(nz, nr, nc, cur.time+1));
-					map[nz][nr][nc] = '#';
-				}
-			}
-		}
-		sb.append("Trapped!").append('\n');
-	}
-	
-	private static boolean isValid(int z, int r, int c) {
-		if (z > -1 && z < L && r > -1 && r < R && c > -1 && c < C) {
-			if (map[z][r][c] != '#') {
-				return true;
-			}
-		}
-		return false;
-	}
+        while (true){
+            st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken()); // 층
+            int R = Integer.parseInt(st.nextToken()); // 행
+            int C = Integer.parseInt(st.nextToken()); // 열
+
+            if (L == 0 && R == 0 && C == 0) break;
+
+            map = new char[L][R][C];
+            visited = new boolean[L][R][C];
+
+            que.clear();
+            for (int i=0; i<L; i++){
+                for (int j=0; j<R+1; j++){
+                    String str = br.readLine();
+                    if(str.equals("")) continue;
+                    for (int k=0; k<C; k++){
+                        char ch = str.charAt(k);
+                        map[i][j][k] = ch;
+                        if(ch == 'S'){
+                            que.offer(new int[] {i, j, k});
+                            visited[i][j][k] = true;
+                        }
+                    }
+                }
+            }
+
+            int res = bfs(L, R, C);
+            if (res == -1){
+                System.out.println("Trapped!");
+            } else {
+                System.out.println("Escaped in "+ res +" minute(s).");
+            }
+        }
+    }
+
+    public static int bfs(int L, int R, int C){
+
+        int time = 0;
+        while(!que.isEmpty()){
+            int size = que.size();
+
+            for (int i=0; i<size; i++) {
+                int[] cur = que.poll();
+                if (map[cur[0]][cur[1]][cur[2]] == 'E') {
+                    return time;
+                }
+
+                for (int d = 0; d < 6; d++) {
+                    int nl = cur[0] + dl[d];
+                    int nr = cur[1] + dr[d];
+                    int nc = cur[2] + dc[d];
+
+                    if (nl < 0 || nr < 0 || nc < 0 || nl >= L || nr >= R || nc >= C) continue;
+                    if (visited[nl][nr][nc] || map[nl][nr][nc] == '#') continue;
+                    que.offer(new int[]{nl, nr, nc});
+                    visited[nl][nr][nc] = true;
+                }
+            }
+            time++;
+        }
+        return -1;
+    }
 }
