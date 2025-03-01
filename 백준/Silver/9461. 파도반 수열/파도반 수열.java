9f0d3e94d25bfa0ce16add20e6dbabb1a13543c2
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-03-02 02:57:56 +0900

    [Silver III] Title: 파도반 수열, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,37 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-// 백준 - 계단오르기
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		long[] p = new long[101];
-		
-		p[1] = 1;
-		p[2] = 1;
-		p[3] = 1;
-		p[4] = 2;
-		p[5] = 2;
-		
-		for (int i=6; i<101; i++) {
-			p[i] = p[i-1] + p[i-5];
-		}
-		
-		for (int tc = 0; tc<T; tc++) {
-			int N = Integer.parseInt(br.readLine());
-			
-			sb.append(p[N]).append("\n");
-		}
-		System.out.println(sb);
-	}
+        long[] p = new long[101];
+        p[1] = 1;
+        p[2] = 1;
+        p[3] = 1;
+
+        for (int i=4; i<101; i++){
+            p[i] = p[i-3] + p[i-2];
+        }
+
+        for (int tc=0; tc<T; tc++){
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(p[n]);
+        }
+    }
 }
