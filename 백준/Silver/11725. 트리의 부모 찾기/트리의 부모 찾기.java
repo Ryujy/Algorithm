a2e9184899451043bdb6709cb9391f441dda5b60
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-03-05 11:24:53 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 528 ms, Memory: 86852 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,50 +2,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer>[] tree;
-    static int[] parents;
+    static int N;
     static boolean[] visited;
-
+    static int[] parent;
+    static ArrayList<Integer>[] adj;
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        parents = new int[n+1];
-        visited = new boolean[n+1];
 
-        tree = new ArrayList[n+1];
-
-        for (int i=0; i<n+1; i++){ // arrayList 초기화
-            tree[i] = new ArrayList<>();
+        adj = new ArrayList[N+1];
+        for(int i = 1; i <= N; i++) {
+            adj[i] = new ArrayList<>();
         }
 
-        for (int i=1; i<n; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+        visited = new boolean[N+1];
+        for(int i = 1; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            int x = Integer.parseInt(input[0]);
+            int y = Integer.parseInt(input[1]);
 
-            tree[a].add(b);
-            tree[b].add(a);
+            adj[x].add(y);
+            adj[y].add(x);
+
         }
 
-        // 루트 노트 : 1
-        dfs(1, 0);
-        for (int i=2; i<n+1; i++){
-            System.out.println(parents[i]);
+        parent = new int[N+1];
+        dfs(1);
+
+        for(int i = 2; i <= N; i++) {
+            sb.append(parent[i]).append("\n");
         }
+
+        System.out.println(sb);
+
     }
 
-    private static void dfs(int node, int parent){
-        parents[node] = parent;
-        visited[node] = true;
+    static void dfs(int index) {
+        visited[index] = true;
 
-        for (int child : tree[node]){
-            if (child == parent || visited[child]) continue;
-            dfs(child, node);
+        for(int i : adj[index]) {
+            if(!visited[i]) {
+                parent[i] = index;
+                dfs(i);
+            }
+            
         }
+
     }
+
 }
