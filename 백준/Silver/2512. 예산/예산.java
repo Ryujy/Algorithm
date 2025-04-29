f818cf7d62311bd5206fd8da76779883b8d483a8
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-04-29 11:21:32 +0900

    [Silver II] Title: 예산, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,36 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main{ // 2512. 예산
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		int left=0, right=-1;
-		int[] arr = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			right = Math.max(right, arr[i]);
-		}
-        
-		int m = Integer.parseInt(br.readLine());
-		while(left<=right) {
-			int mid = (left+right)/2;
-			long budget =0;
-			for(int i=0; i<n; i++) {
-				if(arr[i]>mid) budget += mid;
-				else budget+= arr[i];
-			}
-			if(budget<=m) {
-				left = mid+1;
-			}else {
-				right = mid-1;
-			}
-		}
-		System.out.println(right);
-	}
+        int n = Integer.parseInt(br.readLine()); // 지방의 수
+        int[] cities = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine()); // 각 지방의 예산 요청
+        for (int i=0; i<n; i++){
+            cities[i] = Integer.parseInt(st.nextToken());
+        }
+        int m = Integer.parseInt(br.readLine()); // 총 예산
+
+        Arrays.sort(cities);
+        int lt = 0;
+        int rt = cities[n-1];
+        int res = 0;
+
+        while (lt <= rt){
+            int mid = (lt + rt)/2;
+            int total = 0;
+
+            for (int c: cities){
+                total += Math.min(c, mid);
+            }
+            if (total > m){
+                rt = mid -1;
+            } else {
+                lt = mid +1;
+                res = mid;
+            }
+        }
+        System.out.println(res);
+    }
 }
