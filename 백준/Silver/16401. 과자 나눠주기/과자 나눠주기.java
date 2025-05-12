e3556a991455e05f01fc27d66ecb4936f94a06b6
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-05-12 22:23:40 +0900

    [Silver II] Title: 과자 나눠주기, Time: 876 ms, Memory: 171340 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -1,52 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 백준 - 과자 나눠주기
-public class Main {
+class Main{ // 16401. 과자 나눠주기
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static int N, M, max;
-	static int[] snack;
+        int m = Integer.parseInt(st.nextToken()); // 조카의 수
+        int n = Integer.parseInt(st.nextToken()); // 과자의 수
+        int[] snacks = new int[n];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++){
+            snacks[i] = Integer.parseInt(st.nextToken());
+        }
 
-		M = Integer.parseInt(st.nextToken()); //조카의 수
-		N = Integer.parseInt(st.nextToken()); // 과자의 수
-		
-		snack = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<N; i++) {
-			snack[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(snack);
-		
-		long left = 1;
-		long right = snack[N-1];
-		long h = 0;
-		
-		while(left <= right) {
-			long mid = (left + right) / 2;
-			
-			int cnt = 0;
-			for (int i=0; i<N; i++) {
-				cnt += snack[i] / mid;
-			}
-			
-			if (cnt >= M) {
-				if (h < mid) h = mid;
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-		
-		System.out.println(h);
-		
-	}
+        Arrays.sort(snacks);
+        // 막대 과자의 길이를 이분탐색 -> 최대 길이
+        int lt = 1;
+        int rt = snacks[n-1];
+        int maxL = 0;
+
+        while(lt <= rt){
+            int mid = (lt+rt)/2;
+            int cnt = 0;
+            for (int i=0; i<n; i++){
+                cnt += snacks[i]/mid;
+            }
+            if(cnt < m){
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+                maxL = mid;
+            }
+        }
+        System.out.println(maxL);
+    }
 }
