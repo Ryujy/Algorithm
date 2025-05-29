abbcf90adce151a0f589a29fb88744744e931a67
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-05-29 23:13:28 +0900

    [Gold V] Title: 로봇 청소기, Time: 72 ms, Memory: 11984 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,82 +1,69 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import static java.lang.Integer.*;
+import static java.lang.Math.*;
 
-// [BOJ] 로봇청소기
-public class Main {
-    public static int[] dr = {-1, 0, 1, 0}; // 북동남서(상우하좌) - 반시계 : idx--
-    public static int[] dc = {0, 1, 0, -1};
+class Main{ // 14503. 로봇 청소기
 
-    public static void main(String[] args) throws IOException{
+    static int n, m;
+    static int size = 1;
+    static int[][] map;
+    static int[] dr = {-1, 0, 1, 0}; // 북,동,남,서
+    static int[] dc = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // r
-        int M = Integer.parseInt(st.nextToken()); // c
-        int[][] room = new int[N][M];
-        int[] robot = new int[3];
 
-        st = new StringTokenizer(br.readLine()); // 로봇의 r, c, d
-        for (int i=0; i<3; i++){
-            robot[i] = Integer.parseInt(st.nextToken());
-        }
+        n = parseInt(st.nextToken());
+        m = parseInt(st.nextToken());
+        map = new int[n][m];
 
-        for (int i=0; i<N; i++){
+        st = new StringTokenizer(br.readLine());
+        int r = parseInt(st.nextToken()); // 최초의 행
+        int c = parseInt(st.nextToken()); // 최초의 열
+        int d = parseInt(st.nextToken()); // 최초의 방향 북,동,남,서
+
+        for (int i=0; i<n; i++){ // 1: 벽, 0: 청소X
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<M; j++){
-                room[i][j] = Integer.parseInt(st.nextToken());
+            for (int j=0; j<m; j++){
+                map[i][j] = parseInt(st.nextToken());
             }
-        } // input room
+        } // map input
 
-        int cnt = 0;
-        int cr = robot[0];
-        int cc = robot[1];
-        L: while(true){
-            // 1. 현재 칸 청소
-            if (room[cr][cc] == 0){
-//                System.out.printf("청소 : %d, r : %d, c: %d \n", room[cr][cc], cr, cc);
-                room[cr][cc] = 2; // 1 - 벽  2- 청소한 칸
-                cnt ++;
-            }
-
-//            System.out.println(cnt);
-//            for (int i=0; i<N; i++){
-//                for (int j=0; j<M; j++){
-//                    System.out.print(room[i][j] + " ");
-//                }
-//                System.out.println();
-//            }
-
-
-            // 주변 4칸 중 청소할 칸 확인
-            for (int d=0; d<4; d++){ // 현재 방향에서 반시계로 확인
-                int rd = (robot[2] + 3) % 4;
-                int nr = cr + dr[rd];
-                int nc = cc + dc[rd];
-                if (room[nr][nc] == 0){ // 3. 청소할 칸 O -> 반시계 90도 회전, 전진
-                    robot[2] = rd;
-                    cr = nr;
-                    cc = nc;
-                    break; // 1.로 돌아감
-                } else if (d < 3){
-//                    System.out.printf("d : %d \n", d);
-                    robot[2] = rd;
-                } else { // 2. 4칸 중 청소할 칸 X -> 후진
-                    robot[2] = rd; /* 틀린 부분 : 반시계로 확인하며 돌아갔는데 후진할 땐 확인 전의 방향키로 후진해야함. */
-//                    System.out.printf("후진 (%d,%d) \n", cr, cc);
-//                    System.out.printf("후진 : %d \n", robot[2]);
-                    nr = cr - dr[robot[2]];
-                    nc = cc - dc[robot[2]];
-//                    System.out.printf("후진 (%d,%d) \n", nr, nc);
-                    if (room[nr][nc] == 1){ //후진 불가능
-                        break L;
-                    }
-                    cr = nr;
-                    cc = nc;
-                    break ;
-                }
-            }
-        }
-        System.out.println(cnt);
-        
+        // 로봇청소기가 청소하는 칸의 개수를 구하기
+        clean(r, c, d);
+        System.out.println(size);
     }
 
+    static void clean(int r, int c, int d){
+        // 1. 현재 칸이 청소되지 않은 경우,
+        map[r][c] = -1; // 현재 칸을 청소한다.
+        for (int i=0; i<4; i++){
+            d = (d+3)%4; // 3-1. 반시계 방향 회전
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+             // 3. 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 있는 경우,
+            if (isValid(nr, nc) && map[nr][nc] == 0){
+            // 3-2. 바라보는 방향의 앞쪽 칸 청소X면 전진
+                size++;
+                clean(nr, nc, d);
+                return; // *****
+            }
+        }
+        // 2. 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우,
+        // 2-1. 바라보는 방향 유지,
+        int bd = (d+2)%4;
+        int br = r + dr[bd];
+        int bc = c + dc[bd];
+        if (isValid(br,bc) && map[br][bc] != 1){ // 후진 가능하면 후진
+            clean(br, bc, d);
+        }
+    }
+
+    static boolean isValid(int r, int c){
+        return 0 < r && r < n-1 && 0 < c && c < m-1;
+    }
 }
