cb6b154b84c7e21b6a8e4d771cbfed61dcd4b165
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-06-04 23:56:48 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 444 ms, Memory: 63060 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,40 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map.Entry;
 import java.util.StringTokenizer;
 
-// 백준 - 포켓몬 마스터
-public class Main {
+import static java.lang.Integer.parseInt;
 
-	static int N, M;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		HashMap<Object, Object> map = new HashMap<>();
-		
-		for (int i=1; i<N+1; i++) {
-			String name = br.readLine();
-			map.put(name, i);
-			map.put(String.valueOf(i), name);
-		}
-		
-		for (int j=0; j<M; j++) {
-			sb.append(map.get(br.readLine())).append("\n");
-		}
-		
-		System.out.println(sb);
-	}
-		
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = parseInt(st.nextToken());
+        int m = parseInt(st.nextToken());
+
+        HashMap<String, String> dic = new HashMap<>();
+
+        for (int i=1; i<n+1; i++){
+            String name = br.readLine();
+            dic.put(String.valueOf(i), name);
+            dic.put(name, String.valueOf(i));
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int j=0; j<m; j++){
+            String q = br.readLine();
+            sb.append(dic.get(q)).append('\n');
+        }
+        System.out.println(sb);
+    }
 }
