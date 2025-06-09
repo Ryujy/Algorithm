bd243c42b36127dcac3bfac33ba81aa86ac09f74
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-06-09 22:38:07 +0900

    [Silver III] Title: 두 수의 합, Time: 264 ms, Memory: 28012 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -4,40 +4,37 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// BOJ_3273 두 수의 합
-public class Main {
+import static java.lang.Integer.parseInt;
 
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        int n = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[n];
+        int n = parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
         for (int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = parseInt(st.nextToken());
         }
+        int x = parseInt(br.readLine());
         Arrays.sort(arr);
+
+        int lt = 0;
+        int rt = n-1;
+        int cnt = 0;
         // 1 2 3 5 7 9 10 11 12
-        int x = Integer.parseInt(br.readLine());
-
-        int lt = 0, rt = n-1, sum = 0, cnt = 0;
-
-        while(lt < rt){
-            sum = arr[lt] + arr[rt];
-            if(sum == x) {
-                cnt++;
+        while (lt < rt){
+            int sum = arr[lt] + arr[rt];
+            if(sum <= x){
+                if (sum == x){
+                    cnt++;
+                }
                 lt++;
-                rt--;
-            } else if(sum > x){
-                rt--;
             } else {
-                lt++;
+                rt--;
             }
         }
-
         System.out.println(cnt);
     }
-
 }
