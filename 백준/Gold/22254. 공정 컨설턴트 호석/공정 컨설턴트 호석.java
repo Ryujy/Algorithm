9d30186c6e894f7c320a7f9af2e1a919da322ff3
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-06-10 11:11:20 +0900

    [Gold III] Title: 공정 컨설턴트 호석, Time: 1352 ms, Memory: 99496 KB -BaekjoonHub

---
+++ 백준/Gold/22254. 공정 컨설턴트 호석/공정 컨설턴트 호석.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+
+public class Main { //https://www.acmicpc.net/problem/22254
+    static int n;
+    static long x;
+    static int[] time;
+    static PriorityQueue<Long> pq;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = parseInt(st.nextToken()); // 선물 주문의 개수
+        x = parseLong(st.nextToken()); // 제작 완료까지 남은 시간
+        st = new StringTokenizer(br.readLine());
+        time = new int[n]; // 선물에 필요한 공정 시간
+        for (int i=0; i<n; i++){
+            time[i] = parseInt(st.nextToken());
+        }
+        // x시간 이내에 모든 선물을 제작하기 위해 필요한 최소 공정 라인 개수 출력
+        // 특정 라인 개수이면 x시간 안에 제작 가능한가?
+        int lt = 1, rt = n; // 공정 라인의 최소, 최대
+        int k = MAX_VALUE;
+        while(lt<=rt){
+            int mid = (lt+rt)/2;
+
+            if (run(mid)){ // mid <= x
+                k = Math.min(mid, k);
+                rt = mid -1;
+            } else {
+                lt = mid +1;
+            }
+        }
+        System.out.println(k);
+    }
+
+    static boolean run(int m){
+        pq = new PriorityQueue<>((x, y) -> Long.compare(x, y));
+        for (int i=0; i<m; i++){ //공정라인 초기값 세팅
+            pq.add(0L);
+        }
+        for (int i=0; i<n; i++){
+            if (!pq.isEmpty()){
+                long tmp = pq.poll();
+                tmp += time[i];
+                pq.add(tmp);
+            }
+        }
+        long res = 0;
+
+        while (!pq.isEmpty()) {
+            res = pq.poll();
+        }
+        return res <= x;
+    }
+}
