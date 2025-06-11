431b23925fd4cc3d8c3e5255ec2211949b69d6ba
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-06-11 10:43:34 +0900

    [Silver IV] Title: 수 찾기, Time: 424 ms, Memory: 50716 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,46 +4,41 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+
 public class Main {
+    static long[] nums;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = parseInt(br.readLine());
+        nums = new long[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++){
+            nums[i] = parseLong(st.nextToken());
+        } // n개의 정수
+        Arrays.sort(nums);
 
-	static StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        int m = parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<m; i++){ // 주어지는 수가 nums에 존재하는지 확인하기
+            long num = parseLong(st.nextToken());
+            sb.append(check(num)).append('\n');
+        }
+        System.out.println(sb);
+    }
 
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		int[] nums = new int[N];
-
-		st = new StringTokenizer(br.readLine());
-		for (int n = 0; n < N; n++) {
-			nums[n] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(nums);
-
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for (int m = 0; m < M; m++) {
-			int ans = 0;
-			int num = Integer.parseInt(st.nextToken());
-
-			sb.append(divide(N, num, nums, 0, N - 1)).append('\n');
-		}
-		System.out.println(sb);
-	}
-
-	private static int divide(int N, int num, int[] nums, int low, int high) {
-		// binarySearch
-		while (low <= high) {
-			int mid = (low + high) / 2;
-			if (num == nums[mid]) {
-				return 1;
-			} else if (num < nums[mid]){
-				high = mid - 1;
-			} else {
-				low = mid + 1;
-			}
-		}
-		return 0;
-	}
-
+    static int check(long num){
+        int lt = 0;
+        int rt = nums.length - 1;
+        while(lt <= rt){
+            int mid = (lt+rt)/2;
+            if(nums[mid] == num) return 1;
+            else if(nums[mid] < num) lt = mid + 1;
+            else rt = mid -1;
+        }
+        return 0;
+    }
 }
