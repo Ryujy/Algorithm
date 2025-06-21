345fbac494b4f5265d303d164a88d8bff0cf2b2b
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-06-21 11:15:22 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -7,15 +7,7 @@ import static java.lang.Integer.parseInt;
 
 public class Main {
 
-    static class result{
-        int zero;
-        int one;
-        result(int zero, int one){
-            this.zero = zero;
-            this.one = one;
-        }
-    }
-    static result[] fibo = new result[41];
+    static int[][] fibo = new int[41][2];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -25,19 +17,22 @@ public class Main {
             int n = parseInt(br.readLine());
 
             // 출력: 0의 횟수 | 1의 횟수
-            sb.append(fibo[n].zero).append(" ").append(fibo[n].one).append('\n');
+            sb.append(fibo[n][0]).append(" ").append(fibo[n][1]).append('\n');
         }
         System.out.println(sb);
     }
 
     static void dp(){
-        fibo[0] = new result(1, 0);
-        fibo[1] = new result(0, 1);
+        fibo[0][0] = 1;
+        fibo[0][1] = 0;
+        fibo[1][0] = 0;
+        fibo[1][1] = 1;
 
         for (int i=2; i<41; i++){
-            int z = fibo[i-1].zero + fibo[i-2].zero;
-            int o = fibo[i-1].one + fibo[i-2].one;
-            fibo[i] = new result(z, o);
+            int z = fibo[i-1][0] + fibo[i-2][0];
+            int o = fibo[i-1][1] + fibo[i-2][1];
+            fibo[i][0] = z;
+            fibo[i][1] = o;
         }
     }
 }
