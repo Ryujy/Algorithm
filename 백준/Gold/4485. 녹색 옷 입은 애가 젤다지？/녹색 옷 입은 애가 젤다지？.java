796c0b0bc3aa24649e8baa4035222cec645d87f0
Ryujy <63836145+Ryujy@users.noreply.github.com>
2025-08-08 18:22:28 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 192 ms, Memory: 18548 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -2,93 +2,84 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import static java.lang.Integer.*;
 
 public class Main {
-	
-	static int N, minCost;
-	static int [][] map;
-	static boolean[][] v;
-	static int[] dr = {-1,1,0,0}; //상하좌우
-	static int[] dc = {0,0,-1,1}; //상하좌우
-	static StringBuilder sb = new StringBuilder();
+    static class Link implements Comparable<Link>{
+        int r;
+        int c;
+        int cost;
+        public Link(int r, int c, int cost) {
+            this.r = r;
+            this.c = c;
+            this.cost = cost;
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int tc = 0;
-		while(true) {
-			tc++;
-			N = Integer.parseInt(br.readLine());
-			if (N == 0) {
-				break;
-			}
-			map = new int[N][N];
-			v = new boolean[N][N];
-			
-			for (int i=0; i<N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j=0; j<N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			minCost = Integer.MAX_VALUE;
-			bfs();
-			sb.append("Problem ").append(tc).append(": ").append(minCost);
-			sb.append("\n");	
-			
-		}
-//			System.out.println("Problem "+tc+": "+minCost);
-		System.out.println(sb);
-	}
+        @Override
+        public int compareTo(Link o) {
+            return compare(this.cost, o.cost);
+        }
+    }
+    static int n;
+    static int[] dr = {0, 0, -1, 1};
+    static int[] dc = {-1, 1, 0, 0};
+    static int[][] cave, visited;
 
-	private static void bfs() {
-		PriorityQueue<Data> pq = new PriorityQueue<>((Data o1, Data o2) ->{
-			return o1.cost - o2.cost;
-		});
-		pq.offer(new Data(0,0,map[0][0]));
-		v[0][0] = true;
-		
-		while(!pq.isEmpty()) {
-			Data cur = pq.poll();
-			v[cur.r][cur.c] = true;
-			if (cur.r == N-1 && cur.c == N-1) {
-				minCost = cur.cost;
-				return;
-			}
-			
-			for (int d=0; d<4; d++) {
-				int nr = cur.r + dr[d];
-				int nc = cur.c + dc[d];
-				int cost = cur.cost;
-				
-				if (isValid(nr, nc) && !v[nr][nc]) {
-					int plus = map[nr][nc];
-					pq.offer(new Data(nr, nc, cost+plus));
-				}
-			}
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        while (true){
+            n = parseInt(br.readLine()); // 동굴의 크기
+            if(n == 0) break;
+            cnt++;
+            cave = new int[n][n];
+            for (int i=0; i<n; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j=0; j<n; j++){
+                    cave[i][j] = parseInt(st.nextToken());
+                }
+            } // end cave
 
-	private static boolean isValid(int nr, int nc) {
-		if (nr > -1 && nr < N && nc > -1 && nc < N) {
-			return true;
-		}
-		return false;
-	}
-	
-	static class Data{
-		int r, c;
-		int cost;
-		public Data(int r, int c, int cost) {
-			this.r = r;
-			this.c = c;
-			this.cost = cost;
-		}
-	}
+            dijkstra();
+            sb.append("Problem ").append(cnt).append(": ");
+            sb.append(visited[n-1][n-1]).append("\n");
+        } // tc end
+        System.out.println(sb);
+
+    }
+    private static void dijkstra(){
+        int INF = MAX_VALUE;
+        visited = new int[n][n];
+        for(int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                visited[i][j] = INF;
+            }
+        }
+        PriorityQueue<Link> pq = new PriorityQueue<>();
+        pq.offer(new Link(0, 0, cave[0][0])); // 초기 시작 (0,0)
+        visited[0][0] = cave[0][0];
+
+        while (!pq.isEmpty()) {
+            Link cur = pq.poll();
+
+            // 탈출 조건
+            if (cur.cost > visited[cur.r][cur.c]) continue;
+            if (cur.c == n-1 && cur.r == n-1) break;
+
+            for (int d=0; d<4; d++){
+                int nr = cur.r + dr[d];
+                int nc = cur.c + dc[d];
+                if (nr > -1 && nr < n && nc > -1 && nc < n){
+                    int ncost = cave[nr][nc] + visited[cur.r][cur.c];
+                    if (ncost < visited[nr][nc]){
+                        visited[nr][nc] = ncost;
+                        pq.offer(new Link(nr, nc, ncost));
+                    }
+                }
+            }
+        }
+    }
 }
